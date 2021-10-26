72a43e49171732eba452e44332e0f0c89e0f87ac
uzma <ubkhan3@gmail.com>
2021-10-26 14:25:08 +0000

    complete all functions

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 //1
 function countToTwenty(){
-  
+  for(let i=1; i<=20; i++){
+      console.log(i);
+  }
 }
 
 //2
 function countFromTwenty(){
-  
+  for(let i=20; i>=1; i--){
+      console.log(i);
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(num){
+  for(let i=1; i<=num; i++){
+      console.log(i);
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(num){
+  for(let i=num; i>=1; i--){
+      console.log(i);
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+   for(let i=1; i<=num;i++){
+       if( i%2 != 0 ){
+           console.log(i);
+       }
+   }
 }
+
+//countToTwenty();
+// countFromTwenty();
+// countToN(10);
+// countFromN(10);
+// countEveryOdd(10);
